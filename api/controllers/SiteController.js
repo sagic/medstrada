31b7f60167afbe1bb01ed9b31b6d71127b4a8de9
chizik <sagi.chizik@gmail.com>
2015-10-27 11:33:12 +0200

    another openshift test - hook

--- api/controllers/SiteController.js
+++ api/controllers/SiteController.js
@@ -5,11 +5,33 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import { match, RoutingContext } from 'react-router'
 import routes from '../../components/front/routes'
 
 module.exports = {
   page: function (req, res) {
-    renderTo(routes, false, res, req.url, {title: 'Medstrada'}, {});
+
+    res.send(200, renderToString(<RoutingContext {...renderProps} />));
+    return;
+
+    match({routes, location: req.url}, (error, redirectLocation, renderProps) => {
+      if (error) {
+        res.json({err: '1'});
+        //res.send(500, error.message)
+      } else if (redirectLocation) {
+        res.json({err: '2'});
+        //res.redirect(302, redirectLocation.pathname + redirectLocation.search)
+      } else if (renderProps) {
+        //renderTo(routes, false, res, '/', {title: 'Medstrada'}, {});
+      } else {
+        res.json({err: '3'});
+        //res.send(404, 'Not found')
+      }
+    });
+
+
   }
 };
 
